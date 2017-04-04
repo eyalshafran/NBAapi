f411cfb619bcda18e0bcc97e7f38966de427b4f2
eyalshafran <shafrane@gmail.com>
2017-04-03 23:04:41 -0600

    Updated plotting

--- NBAapi/plot.py
+++ NBAapi/plot.py
@@ -1,8 +1,9 @@
+#%%
 from matplotlib.patches import Circle, Rectangle, Arc
 import matplotlib.pyplot as plt
 import numpy as np
 
-def court(ax=None, color='black', lw=2, outer_lines=False):
+def court(ax=None, color='black', lw=4, outer_lines=False,direction='up'):
     '''
     Plots an NBA court
     outer_lines - accepts False or True. Plots the outer side lines of the court.
@@ -10,16 +11,22 @@ def court(ax=None, color='black', lw=2, outer_lines=False):
     '''
     # If an axes object isn't provided to plot onto, just get current one
     if ax is None:
-        ax = plt.gca(xlim = [-30,30],ylim = [40,-10],xticks=[],yticks=[],aspect=1.0)
-
+        if direction=='up':
+            ax = plt.gca(xlim = [-30,30],ylim = [43,-7],xticks=[],yticks=[],aspect=1.0)
+            plt.text(22,44,'By: Doingthedishes',color='blue',horizontalalignment='center',fontsize=20)
+        elif direction=='down':
+            ax = plt.gca(xlim = [30,-30],ylim = [-7,43],xticks=[],yticks=[],aspect=1.0)
+            plt.text(-22,-7,'By: Doingthedishes',color='blue',horizontalalignment='center',fontsize=20)
+        else:
+            ax = plt.gca()
     # Create the various parts of an NBA basketball court
 
     # Create the basketball hoop
     # Diameter of a hoop is 1.5 
-    hoop = Circle((0, 0), radius=1.5, linewidth=lw/2, color=color, fill=False)
+    hoop = Circle((0, 0), radius=0.75, linewidth=lw/2, color=color, fill=False)
 
     # Create backboard
-    backboard = Rectangle((-3, -1.5), 6, -0.1, linewidth=lw, color=color)
+    backboard = Rectangle((-3, -0.75), 6, -0.1, linewidth=lw, color=color)
 
     # The paint
     # Create the outer box of the paint, width=16ft, height=19ft
@@ -54,15 +61,20 @@ def court(ax=None, color='black', lw=2, outer_lines=False):
 
     if outer_lines:
         # Draw the half court line, baseline and side out bound lines
-        outer_lines = Rectangle((-25, -5.25), 50, 52.25, linewidth=lw,
+        outer_lines = Rectangle((-25, -5.25), 50, 46.75, linewidth=lw,
                                 color=color, fill=False)
+        center_outer_arc = Arc((0, 41.25), 12, 12, theta1=180, theta2=0,
+                                linewidth=lw, color=color)
+        center_inner_arc = Arc((0, 41.25), 4, 4, theta1=180, theta2=0,
+                                linewidth=lw, color=color)
         court_elements.append(outer_lines)
+        court_elements.append(center_outer_arc)
+        court_elements.append(center_inner_arc)
     else:
         plt.plot([-25,25],[-5.25,-5.25],linewidth=lw,color=color)
     # Add the court elements onto the axes
     for element in court_elements:
         ax.add_patch(element)
-    plt.text(0,38,'By: Doingthedishes',color='black',horizontalalignment='center',fontsize=20)
 #    ax.axis('off')
     return ax
 
@@ -97,11 +109,11 @@ def zones(**kwargs):
     ax.plot([np.cos(ang/180*np.pi)*16, np.cos(ang/180*np.pi)*23.75],
              [np.sin(ang/180*np.pi)*16, np.sin(ang/180*np.pi)*23.75],**kwargs)
     ang = 72.0
-    ax.plot([np.cos(ang/180*np.pi)*24, np.cos(ang/180*np.pi)*47],
-             [np.sin(ang/180*np.pi)*24, 47],**kwargs)
+    ax.plot([np.cos(ang/180*np.pi)*24, np.cos(ang/180*np.pi)*41.25],
+             [np.sin(ang/180*np.pi)*24, 41.25],**kwargs)
     ang = 72.0+36.0
-    ax.plot([np.cos(ang/180*np.pi)*24, np.cos(ang/180*np.pi)*47],
-             [np.sin(ang/180*np.pi)*24, 47],**kwargs)
+    ax.plot([np.cos(ang/180*np.pi)*24, np.cos(ang/180*np.pi)*41.25],
+             [np.sin(ang/180*np.pi)*24, 41.25],**kwargs)
         
 def text_in_zone(string,zone,box_alpha = 0.75,**kwargs):
     '''
@@ -113,33 +125,33 @@ def text_in_zone(string,zone,box_alpha = 0.75,**kwargs):
     box_alpha - string box transparency 
     '''
     if zone == ('Less Than 8 ft.','Center(C)') :
-        t = plt.text(0,4,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+        t = plt.text(0,2,string,horizontalalignment='center',verticalalignment='center',**kwargs)
         t.set_bbox(dict(color='white', alpha=box_alpha))
     elif zone[0] ==  '8-16 ft.':
         if zone[1] == 'Center(C)':
             t = plt.text(0,12,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
         elif zone[1] == 'Right Side(R)' :
-            t = plt.text(12,2,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t = plt.text(12,0,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
         elif zone[1] == 'Left Side(L)':
-            t = plt.text(-12,2,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t = plt.text(-12,0,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
     elif zone[0] == '16-24 ft.':
         if zone[1] == 'Center(C)':
             t = plt.text(0,20,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
         elif zone[1] == 'Right Side(R)':
-            t = plt.text(19,6.3,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t = plt.text(18,7.3,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
         elif zone[1] ==  'Left Side(L)':
-            t = plt.text(-19,6.3,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t = plt.text(-18,7.3,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
         elif zone[1] == 'Right Side Center(RC)':
-            t = plt.text(12.5,16.5,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t = plt.text(12,15.5,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
         elif zone[1] == 'Left Side Center(LC)':
-            t = plt.text(-12.5,16.5,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t = plt.text(-12,15.5,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
     elif zone[0] == '24+ ft.':
         if zone[1] == 'Center(C)':
@@ -152,9 +164,12 @@ def text_in_zone(string,zone,box_alpha = 0.75,**kwargs):
             t = plt.text(-27,0,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
         elif zone[1] == 'Right Side Center(RC)':
-            t = plt.text(17,24,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t = plt.text(19,24,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha))
         elif zone[1] == 'Left Side Center(LC)':
-            t = plt.text(-17,24,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t = plt.text(-19,24,string,horizontalalignment='center',verticalalignment='center',**kwargs)
             t.set_bbox(dict(color='white', alpha=box_alpha)) 
+    if zone == ('Back Court Shot', 'Back Court(BC)') :
+            t = plt.text(0,39,string,horizontalalignment='center',verticalalignment='center',**kwargs)
+            t.set_bbox(dict(color='white', alpha=box_alpha))
 
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### NBAapi
 
 This package is used to extract data from the NBA api. It also includes a few plotting features.
+
 It is used for data analysis which can be seen in my blog.
